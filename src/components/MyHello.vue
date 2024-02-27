3041a1b3a5766cab26e13679ba39de841269c8ea
tenggys <levgood21@yandex.ru>
2024-02-27 13:13:09 +0300

    first commit

---
+++ src/components/MyHello.vue
@@ -0,0 +1,25 @@
+<script setup>
+    defineProps({
+        title: String,
+        price: Number,
+        isAdded: Boolean
+    });
+
+</script>
+
+<template>
+    <div class="product">
+        <h2>Товар: {{ title }}</h2>
+        <h3>Цена: {{ price }}</h3>
+        <h3>Добаввлен: {{ isAdded }}</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+    .product {
+        border: 1px solid blue;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+</style>
